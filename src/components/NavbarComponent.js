1b9ef5c1179813d9b2f6f0cafe9ed383914158a8
kundella prasanth yadav <prasanthkdp6@gmail.com>
2020-09-24 10:16:31 +0530

    create navbarcomponent

---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {AppBar,Button,ButtonGroup} from '@material-ui/core'
+
+class Navbar extends React.Component{
+
+   
+    handleTabs=(tab)=>{
+        this.props.tab(tab);
+        
+      }
+    
+    render(){
+        const greyColor={
+            color:"grey"
+        }
+        
+      
+
+        return(
+            <div className="Navbar">
+               <AppBar position="static" id="appbar" >
+               <ButtonGroup size="large" >
+               <Button variant="text"  style={greyColor} >LOGO</Button>
+                 <Button variant="text" style={greyColor} onClick={()=>this.handleTabs("home")}>Home</Button>
+                 <Button variant="text" style={greyColor} onClick={()=>this.handleTabs("tasks")}>Tasks</Button>
+                 <Button variant="text" style={greyColor} onClick={()=>this.handleTabs("user")}>User</Button>
+                </ButtonGroup>
+                
+               </AppBar>
+
+              
+            </div>
+        )
+
+       
+    }
+    
+}
+
+export default Navbar;
